0335d18587423681418913bd3a79c463ee7e77ab
Swan Pyae Sone <joyboyop69@gmail.com>
2025-02-15 21:39:08 +0700

    Changes 1

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
- // Function to load the navbar content (sidebar)
- function loadNavbar() {
-    fetch('navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading navbar:', error));
-  }
+// Function to load the navbar content (sidebar)
+function loadNavbar() {
+  fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+}
 
-  // Function to load content based on clicked item (e.g., Dashboard, Order, Table)
-  function loadPage(page) {
-    fetch('pages/'+page + '.html') // Dynamically load the page based on the link clicked
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('content').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading page:', error));
+// Function to load content based on clicked item (e.g., Dashboard, Order, Table)
+function loadPage(page) {
+  fetch('pages/'+page + '.html') // Dynamically load the page based on the link clicked
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('content').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading page:', error));
+}
 
-  }
-
-  // Load the navbar when the page loads, and set default content
-  window.onload = function() {
-    loadNavbar();           // Load the navbar
-    loadPage('dashboard');  // Load Dashboard by default
-  };
+// Load the navbar when the page loads, and set default content
+window.onload = function() {
+  loadNavbar();           // Load the navbar
+  loadPage('dashboard');  // Load Dashboard by default
+};
